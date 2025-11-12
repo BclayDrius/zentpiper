ebceed791cd402e6844fee3e1e84ec9d00c5631e
zentpiper <zentpiper@gmail.com>
2025-11-11 19:50:20 -0500

    Nuevo footer en contacto

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,18 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import FooterAlt from "./FooterAlt";
 import ScrollToTop from "./ScrollToTop";
+import { useLocation } from "react-router-dom";
 import "./Layout.css";
 
 function Layout({ children }) {
+  const location = useLocation();
+
   return (
     <div className="app-container">
       <Header />
       <main>{children}</main>
-      <Footer />
+      {location.pathname === "/contacto" ? <FooterAlt /> : <Footer />}
       <ScrollToTop />
     </div>
   );
